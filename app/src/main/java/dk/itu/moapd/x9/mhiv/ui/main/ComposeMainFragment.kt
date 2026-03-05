1047a2fe71bb2304fa0fdcd133e2ed05b632cde8
MarcusHVange <marcusvange@gmail.com>
2026-03-05 22:17:24 +0100

    Add theme to Jetpack compose version

--- app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/ComposeMainFragment.kt
+++ app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/ComposeMainFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
@@ -15,6 +19,7 @@ import androidx.navigation.findNavController
 import dk.itu.moapd.x9.mhiv.R
 import dk.itu.moapd.x9.mhiv.ui.composables.MainScreen
 import dk.itu.moapd.x9.mhiv.ui.shared.DataViewModel
+import dk.itu.moapd.x9.mhiv.ui.theme.X9Theme
 
 class ComposeMainFragment : Fragment() {
 
@@ -33,12 +38,19 @@ class ComposeMainFragment : Fragment() {
             setContent {
                 val reports by viewModel.cont.observeAsState(emptyList())
 
-                MainScreen(
-                    reports = reports,
-                    onAddReportNavigate = {
-                        findNavController().navigate(R.id.action_compose_main_to_traffic_report)
+                X9Theme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        MainScreen(
+                            reports = reports,
+                            onAddReportNavigate = {
+                                findNavController().navigate(R.id.action_compose_main_to_traffic_report)
+                            }
+                        )
                     }
-                )
+                }
             }
         }
     }
