c76a9bcfeff5d687ddc55e0561abbb08eb0c331d
MarcusHVange <marcusvange@gmail.com>
2026-03-06 12:47:45 +0100

    Remove Android Framework MainFragment

--- app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/MainFragment.kt
+++ app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/MainFragment.kt
@@ -1,63 +1,65 @@
 package dk.itu.moapd.x9.mhiv.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.findNavController
 import dk.itu.moapd.x9.mhiv.R
-import dk.itu.moapd.x9.mhiv.databinding.FragmentMainBinding
-import dk.itu.moapd.x9.mhiv.ui.list.CustomAdapter
+import dk.itu.moapd.x9.mhiv.ui.composables.MainScreen
 import dk.itu.moapd.x9.mhiv.ui.shared.DataViewModel
-import dk.itu.moapd.x9.mhiv.ui.utils.viewBinding
+import dk.itu.moapd.x9.mhiv.ui.theme.X9Theme
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private val binding by viewBinding(FragmentMainBinding::bind)
+
     private val viewModel: DataViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+            )
+
+            setContent {
+                val reports by viewModel.cont.observeAsState(emptyList())
+
+                X9Theme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        MainScreen(
+                            reports = reports,
+                            onAddReportNavigate = {
+                                findNavController().navigate(R.id.action_compose_main_to_traffic_report)
+                            },
+                            onDelete = { index ->
+                                viewModel.deleteCont(index)
+                            }
+                        )
+                    }
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val adapter = CustomAdapter(emptyList())
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerView.adapter = adapter
-
-        viewModel.cont.observe(viewLifecycleOwner) { reportData ->
-            adapter.submitList(reportData)
-        }
 
         viewModel.reportCreated.observe(viewLifecycleOwner) { wasCreated ->
             if (wasCreated) {
@@ -65,29 +67,5 @@ class MainFragment : Fragment() {
                 viewModel.onReportCreatedToastShown()
             }
         }
-
-        binding.goToReportBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_main_to_traffic_report)
-        }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
