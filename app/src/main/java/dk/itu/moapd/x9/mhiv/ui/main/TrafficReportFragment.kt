64c6005e5c47f9dbddfc41bbd5c0699210404362
MarcusHVange <marcusvange@gmail.com>
2026-03-20 15:10:34 +0100

    Remove unnecessary code from TrafficReportFragment

--- app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/TrafficReportFragment.kt
+++ app/src/main/java/dk/itu/moapd/x9/mhiv/ui/main/TrafficReportFragment.kt
@@ -16,38 +16,10 @@ import dk.itu.moapd.x9.mhiv.domain.model.TrafficReportModel
 import dk.itu.moapd.x9.mhiv.ui.shared.DataViewModel
 import dk.itu.moapd.x9.mhiv.ui.utils.viewBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TrafficReportFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TrafficReportFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class TrafficReportFragment : Fragment(R.layout.fragment_traffic_report) {
     private val binding by viewBinding(FragmentTrafficReportBinding::bind)
+
     private val viewModel: DataViewModel by activityViewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_traffic_report, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -65,15 +37,6 @@ class TrafficReportFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TrafficReportFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         private const val KEY_REPORT_TITLE = "key_report_title"
         private const val KEY_REPORT_TYPE = "key_report_type"
         private const val KEY_REPORT_DESCRIPTION = "key_report_description"
@@ -82,15 +45,6 @@ class TrafficReportFragment : Fragment() {
         private const val KEY_TYPE_ERROR = "key_type_error"
         private const val KEY_DESCRIPTION_ERROR = "key_description_error"
         private const val KEY_PRIORITY_ERROR = "key_priority_error"
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TrafficReportFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     private fun setupUI() {
